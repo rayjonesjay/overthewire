f842b17d17f5a0dfe899027618541994e2a58869
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-22 21:32:41 +0300

    level0 solution

--- bandit/level0.sh
+++ bandit/level0.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 . variables.sh
-password="ZjLjTmM6FvvyRnrb2rfNWOZOTa6ip5If"
-echo "password to level0-> $password"
+echo "password to level0-> bandit0"
 echo "connecting...to $host via port $port"
 ssh -p 2220 bandit0@bandit.labs.overthewire.org
---
+++ bandit/variables.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+host="bandit0@bandit.labs.overthewire.org"
+port="2220"
