aab1d0905f9816ab21624c5e67e010b8d39ed2dd
rayjonesjay <rayjaymuiruri@gmail.com>
2024-08-22 21:59:21 +0300

    level0

--- bandit/level0.sh
+++ bandit/level0.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
-level="level0"
-level0_pass="bandit0"
-echo -e "password to ${level} is $level0_pass\n"
-. variables.sh
+#!/usr/bin/env bash
+function connect(){
+    local host="bandit0@bandit.labs.overthewire.org"
+    local port="2220"
+    local level="level0"
+    local level0_pass="bandit0"
+    echo -e "password to ${level} is $level0_pass\n"
+    echo "connecting to $host via port $port..."
+    ssh -p ${port} ${host}
+}
+
 connect
